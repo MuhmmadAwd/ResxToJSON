6b3e2b352819dbc4ce967f24ff74c56141bd51f1
Muhmmad Awd <41266552+MuhmmadAwd@users.noreply.github.com>
2022-08-22 11:06:14 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ResxToJSON
+{
+    class Program
+    {
+        static string Divisor = "---------------------";
+
+        static void Main(string[] args)
+        {
+            int Errors = 0;
+            var listFiles = Directory.GetFiles(AppContext.BaseDirectory, "*.resx", SearchOption.AllDirectories).ToList();
+
+            Console.WriteLine($"Arquivos encontrados: {listFiles.Count}");
+            Console.WriteLine(Divisor);
+
+            foreach (var file in listFiles)
+            {
+                try
+                {
+                    string xmlText = File.ReadAllText(file);
+
+                    var jsonText = "{" + Environment.NewLine;
+
+                    var listData = XElement.Parse(xmlText)
+                                           .Elements("data")
+                                           .ToList();
+
+                    listData.ForEach(delegate (XElement data)
+                    {
+                        var name = data.Attribute("name").Value;
+                        var valor = data.Element("value").Value
+                                     .Trim()
+                                     .Replace('"', '\'');
+
+                        jsonText += $"\t\"{name}\": \"{valor}\",{Environment.NewLine}";
+                    });
+
+                    jsonText += "}";
+
+                    File.WriteAllText(file.Replace(".resx", ".json"), jsonText);
+                    Console.WriteLine($"{file} - OK");
+                }
+                catch
+                {
+                    Console.WriteLine($"{file} - Error");
+                    continue;
+                }
+            }
+
+            Console.WriteLine(Divisor);
+            Console.WriteLine($"{Errors} Arquivos com falha");
+            Console.WriteLine($"{listFiles.Count - Errors} Arquivos com êxito");
+            Console.Read();
+        }
+    }
+}
